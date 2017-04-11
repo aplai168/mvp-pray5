f883dd6406448b58849cfc83803e8c84b97ccc40
Alice Lai <aplai168@gmail.com>
2017-04-11 11:41:49 -0500

    (cleanup)

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const methodOverride = require('method-override');
+// const methodOverride = require('method-override');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -22,12 +22,12 @@ app.use(express.static('./public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride('X-HTTP-Method-Override'));
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(methodOverride('X-HTTP-Method-Override'));
 
 require('./app/routes.js')(app);
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.listen(port);
 console.warn(`App listening on port http://localhost:${port}`);
